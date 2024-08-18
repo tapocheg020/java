c75f18922fecbecbd036fa89749e4adf3ee68304
Ilya <ilya.guchick07@mail.ru>
2024-08-18 15:47:13 +0300

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+public class Application {
+    public static void main(String[] args) {
+        float temp = 36000.788f;
+        double tempTwo = 36000.788;
+        System.out.println(temp);
+        System.out.println(tempTwo);
+
+        System.out.println(temp + ',' + tempTwo);
+        System.out.printf("%s, %s%n", temp, tempTwo);
+
+        
+
+    }
+}
