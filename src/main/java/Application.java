aa47ef45dc69bcb7707ee478cc5837b15d5dde5c
Ilya <ilya.guchick07@mail.ru>
2024-08-24 09:27:03 +0300

    FirstCommit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,44 @@
-public class Application {
-    public static void main(String[] args) {
-        float temp = 36000.788f;
-        double tempTwo = 36000.788;
-        System.out.println(temp);
-        System.out.println(tempTwo);
 
-        System.out.println(temp + ',' + tempTwo);
-        System.out.printf("%s, %s%n", temp, tempTwo);
+class Metods {
+    void createTable(){
+        System.out.println("Создаю самый обычныый стол");
+    }
 
-        
+    void createTable(String color) {
+//        color="red";
+        System.out.printf("Создаю стол %s цвета%n", color);
+    }
+
+    void createTable(String color, int numberOfLegs){
+//        color = "black";
+//        numberOfLegs = 4;
+        System.out.printf("Создаю стол %s цвета с %d ножками%n", color, numberOfLegs);
+    }
+
+
+    void createTable(int numberOfLegs, String color){
+//        color = "white";
+//        numberOfLegs = 2;
+        System.out.printf("такой же метод но теперь я создаю стол %s цвета с %d ножками%n", color, numberOfLegs);
+    }
+    public void callMetods(){
+        createTable();
+        createTable("red");
+        createTable("red", 4);
+        createTable(2, "black");
+
 
     }
+}
+
+public abstract class Application {
+
+    public static void main(String[] args) {
+        Metods obj = new Metods();
+
+        obj.callMetods();
+    }
+
+
+
 }
---
+++ src/main/java/Stack.java
@@ -0,0 +1,15 @@
+public class Stack {
+    public static void main(String[] args) {
+
+    }
+
+    public static void one(){
+        int first = 14;
+        int second = 20;
+        System.out.printf("one: first=%s, second=%s".formatted());
+
+    }
+
+
+
+}
